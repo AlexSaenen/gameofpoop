fb422b7c5ecde91357e75b41fcc627e1dbb141f9
AlexSaenen <alexander.saenen@epitech.eu>
2017-08-15 11:55:59 +0200

    Send file instead of 416

--- server/routes/series/seasons/episodes/single.js
+++ server/routes/series/seasons/episodes/single.js
@@ -64,7 +64,7 @@ const streamEpisode = (videoPath, req, res) => {
     const range = req.headers.range;
 
     if (!range) {
-     return res.sendStatus(416);
+      res.sendFile(videoPath);
     }
 
     const positions = range.replace(/bytes=/, "").split("-");
